b6c82100a0b9311ce0230c3154a6d6a8133f010b
HayGT <haygtx@gmail.com>
2020-05-02 17:31:27 +0300

    refactor(fontawesome): увеличил производительность, убрав импорт лишних стилей и шрифтов которые импортировать из all.css

--- src/Styles/vuetify.js
+++ src/Styles/vuetify.js
@@ -2,7 +2,7 @@ import Vue from '../../node_modules/vue';
 import Vuetify from 'vuetify/lib';
 import { Touch } from 'vuetify/lib/directives';
 
-import '@fortawesome/fontawesome-free/css/all.css' // Не менять, в free скидывать файлы от Pro
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css' // Не менять, в free скидывать файлы от Pro
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify, {
