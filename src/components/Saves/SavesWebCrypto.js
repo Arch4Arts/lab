30ab32a336815882eaea93501191daf6c4d7adf1
HayGT <haygtx@gmail.com>
2020-06-27 20:00:14 +0300

    chore(WebCyrpto)

--- src/components/Saves/SavesWebCrypto.js
+++ src/components/Saves/SavesWebCrypto.js
@@ -1,26 +1,28 @@
 // Генерация уникального ключа на основе имени сохранения
 async function genEncryptionKey(saveName, mode, length) {
-	let algo = {
+	const algoritm = {
 		name: 'PBKDF2',
 		hash: 'SHA-512',
-		salt: new TextEncoder().encode('a-unique-salt-for-save'),
-		iterations: 1000
+		salt: new TextEncoder().encode('3F4428472B4B6250'),
+		iterations: 1
 	};
 	let derived = { name: mode, length: length };
 	let encoded = new TextEncoder().encode(saveName);
 	let key = await crypto.subtle.importKey('raw', encoded, { name: 'PBKDF2' }, false, ['deriveKey']);
 	
-	return crypto.subtle.deriveKey(algo, key, derived, false, ['encrypt', 'decrypt']);
+	return crypto.subtle.deriveKey(algoritm, key, derived, false, ['encrypt', 'decrypt']);
 };
 
 
 export default {
+	algoritm: {
+		name: 'AES-GCM',
+		length: 256,
+		iv: crypto.getRandomValues(new Uint8Array(12))
+	},
 	encrypt: async function (saveName, saveData) {
-		let algo = {
-			name: 'AES-GCM',
-			length: 256,
-			iv: crypto.getRandomValues(new Uint8Array(12))
-		};
+		const algo = this.algoritm;
+
 		let key = await genEncryptionKey(saveName, algo.name, algo.length);
 		let encoded = new TextEncoder().encode(saveData);
 		
@@ -29,14 +31,12 @@ export default {
 			iv: algo.iv
 		};
 	},
-	decrypt: async function (saveName, encryptedData) {
-		let algo = {
-			name: 'AES-GCM',
-			length: 256,
-			iv: encryptedData.iv
-		};
+	decrypt: async function (saveName, encrypted) {
+		const algo = this.algoritm;
+		algo.iv = encrypted.iv;
+
 		let key = await genEncryptionKey(saveName, algo.name, algo.length);
-		let decrypted = await crypto.subtle.decrypt(algo, key, encryptedData.cipherData);
+		let decrypted = await crypto.subtle.decrypt(algo, key, encrypted.cipherData);
 	
 		return new TextDecoder().decode(decrypted);
 	}
