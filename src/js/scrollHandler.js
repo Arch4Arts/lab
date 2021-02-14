4a40f3bbdda2027478bde6d22de681537c584e50
Arch4Arts <arch4arts@gmail.com>
2021-02-14 18:36:33 +0300

    refactor(scrollHandler)

--- src/js/scrollHandler.js
+++ src/js/scrollHandler.js
@@ -1,9 +1,10 @@
 import localforage from 'localforage';
 
-localforage.getItem('scrollPosition').then((y) => window.scrollTo(0, y));
-
-window.onbeforeunload = function() {
-  // https://plainjs.com/javascript/styles/get-and-set-scroll-position-of-an-element-26/
+const getScrollPos = () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   localStorage.setItem('scrollPosition', scrollTop);
 }
+const setScrollPos = () => localforage.getItem('scrollPosition').then((y) => window.scrollTo(0, y));
+
+addEventListener('beforeunload', getScrollPos);
+addEventListener('DOMContentLoaded', setScrollPos);
